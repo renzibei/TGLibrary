3a4567e5578e1f6b620e80b97b67313e2f6ee5df
Fan Qu <fan_qu@icloud.com>
2018-05-27 14:58:10 +0800

    change connect

--- webs/html/testMysql.php
+++ webs/html/testMysql.php
@@ -1,39 +1,39 @@
 <?php
-
+/*
 function customError($errno, $errstr)
  { 
  echo "<b>Error:</b> [$errno] $errstr";
  }
+*/
+
+function getLine($fileHandle)
+{
+	$tempBuffer = fgets($fileHandle);
+	if($tempBuffer == false)
+		return false;
+	$tempBuffer = substr($tempBuffer, 0, -1);
+	return $tempBuffer;
+}
 
 //set error handler
+/*
 set_error_handler("customError");
+*/
 
-//echo dirname(__DIR__) . "<br />";
 $userFilePath =  dirname(__DIR__) . '/dbusers/dbadmin';
-//echo $userFilePath . "<br />";
 if(!file_exists($userFilePath))
  {
 	echo("File not found");
  }
 else
  {
- //$file=fopen($userFilePath,"r");
- //echo "存在" . "<br />";
  	$userFile = fopen($userFilePath, "r"); 
- //$userFile = fopen('./html/test1.html', 'r');
 	if($userFile != false) {
 		echo "读取成功" . '<br />';
-		$db_username = 'library_admin';//fgets($userFile);
-		$db_username2 = fgets($userFile);
-		$db_passwd = 'tiguandaduihenshuai'; //gets($userFile);
-		$db_passwd2 = fgets($userFile);
-		$db_server =  'localhost'; //fgets($userFile);
-		$db_server2 =  fgets($userFile);
-		//echo 'haha';
-		echo '比较' . strcmp($db_server, $db_server2) . ' '  . strcmp($db_username, $db_username2) . ' ' . strcmp($db_passwd, $db_passwd2) . '<br />';
-		echo 'Length' . strlen($db_server) . " " . strlen($db_server2) . "<br />";
-		echo 'db_username: ' . $db_username . ' ' . $db_username2 .  '<br />' . 'db_server: ' . $db_server . ' ' . $db_server2  .'<br />' . 'db_passwd: ' . $db_passwd .' ' . $db_passwd2 . "<br />";
-		$conn = new mysqli($db_server, $db_username2, $db_passwd);
+		$db_username2 = getLine($userFile);
+		$db_passwd2 = getLine($userFile);
+		$db_server2 = getLine($userFile);
+		$conn = new mysqli($db_server2, $db_username2, $db_passwd2);
 		if($conn->connect_error) {
 			die('连接失败：' . $coon->connect_error);
 		}
@@ -43,9 +43,7 @@ else
  }
 
 /*
-$servername = "localhost";
-$username = "library_admin";
-$password = "tiguandaduihenshuai";
+\
  
 // 创建连接
 $conn = new mysqli($servername, $username, $password);
