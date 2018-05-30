05541207031e791844dcf8ed1f4a64c36240346c
Fan Qu <fan_qu@icloud.com>
2018-05-31 02:13:50 +0800

    finish install.php

---
+++ webs/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta http-equiv="Content-Type"; content="text/html"; charset="UTF-8">
+
+<title>菜鸟教程(runoob.com)</title>
+</head>
+<body>
+<?php
+	phpinfo();
+?>
+</body>
+</html>
