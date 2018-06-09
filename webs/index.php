6235e55dcac087e4fd74acc5f21837536da80353
cindyjenaro <No17forever@qq.com>
2018-06-09 22:57:27 +0800

    A lot of modifications on directory.

--- webs/index.php
+++ webs/index.php
@@ -87,13 +87,13 @@
         require_once 'html/retrieveSet.php';
         require_once 'html/RealBook.php';
         \tg\SystemFrame::log_info("finish load inclued");
-	    $book = new \tg\Book('他改变了中国', array('长着'), 'Chinese', 1998,
+	    $book = new \tg\Book('他改变了中国', array('长者'), 'Chinese', 1998,
                 array("哲学", "膜蛤"), "人民出版社", array('www.baidu.com'), 'www.baidu.com', "不要总想搞个大新闻", '2332332', '精装');
         \tg\SystemFrame::log_info("new a book");
 	    \tg\SystemFrame::docData()->addDocument($book);
 	    \tg\SystemFrame::log_info("add book");
         $book2 = \tg\SystemFrame::docData()->getDocument(1);
-        $books = \tg\SystemFrame::docData()->queryDoc(array((new \tg\retrieveTitle('他改变了中国'))->And(), (new \tg\retrieveAuthor('长着'))->And(),
+        $books = \tg\SystemFrame::docData()->queryDoc(array((new \tg\retrieveTitle('他改变了中国'))->And(), (new \tg\retrieveAuthor('长者'))->And(),
                                                         (new \tg\retrieveISBN('2332333'))->Or(), (new \tg\retrieveSubject('膜蛤'))->Not()));
         if(count($books) > 0) {
             $book1 = $books[0];
